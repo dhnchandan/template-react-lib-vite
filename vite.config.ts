20fd923d6da57a6b4d741a4d3fce5633dcd69b40
DHN Chandan <php.chandan@gmail.com>
2024-03-18 10:29:24 +0600

    feat: :wrench: Config vite to publish lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'pathe'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'ReactLib',
+      fileName: 'react-lib',
+      formats: ['cjs', 'es', 'iife', 'umd'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDom',
+        },
+      },
+    },
+  },
   plugins: [react()],
 })
